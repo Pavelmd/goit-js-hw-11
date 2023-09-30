b8a87152e3ad7a0c003ba283809601ee752a8ba6
Pavelmd <pashanovosvit2@gmail.com>
2023-10-01 00:09:28 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,173 @@
-console.log('Hello');
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox = new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.PER_PAGE = 40;
+  }
+  async fetchGallery() {
+    const axiosOptions = {
+      method: 'get',
+      url: 'https://pixabay.com/api/',
+      params: {
+        key: '39763985-b7eed9bb6a1f43525593991b0',
+        q: `${this.searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: `${this.page}`,
+        per_page: `${this.PER_PAGE}`,
+      },
+    };
+    try {
+      const response = await axios(axiosOptions);
+
+      const data = response.data;
+
+      this.incrementPage();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  resetEndOfHits() {
+    this.endOfHits = false;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+let isShown = 0;
+const newsApiService = new NewsApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+const options = {
+  rootMargin: '50px',
+  root: null,
+  threshold: 0.3,
+};
+const observer = new IntersectionObserver(onLoadMore, options);
+
+function onSearch(element) {
+  element.preventDefault();
+
+  refs.galleryContainer.innerHTML = '';
+  newsApiService.query =
+    element.currentTarget.elements.searchQuery.value.trim();
+  newsApiService.resetPage();
+
+  if (newsApiService.query === '') {
+    Notify.warning('Please, fill the main field');
+    return;
+  }
+
+  isShown = 0;
+  fetchGallery();
+  onRenderGallery(hits);
+}
+
+function onLoadMore() {
+  newsApiService.incrementPage();
+  fetchGallery();
+}
+
+async function fetchGallery() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+
+  const result = await newsApiService.fetchGallery();
+  const { hits, total } = result;
+  isShown += hits.length;
+
+  if (!hits.length) {
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+    refs.loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
+
+  onRenderGallery(hits);
+  isShown += hits.length;
+
+  if (isShown < total) {
+    Notify.success(`Hooray! We found ${total} images !!!`);
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  }
+
+  if (isShown >= total) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
+
+function onRenderGallery(elements) {
+  const markup = elements
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+      <a href="${largeImageURL}">
+        <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${downloads}
+        </p>
+      </div>
+      </div>`;
+      }
+    )
+    .join('');
+  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
